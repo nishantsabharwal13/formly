a8e2133a9bda70fd2c0a688a31c282f6fb1e4f9b
Nishant Sabharwal <nishantsabharwal13@gmail.com>
2018-09-17 18:48:15 +0530

    setup rn in progress

---
+++ src/components/header/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  componentWillMount() {
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>hello</Text>
+      </View>
+    )
+  }
+}
+
+export default Header;
